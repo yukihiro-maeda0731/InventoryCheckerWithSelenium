127b5762aa8ad1c1a0b8ccb99cf3ed983132fc35
前田 <sloth19920731@gmail.com>
2022-01-05 22:25:07 +0900

    #2の6:30あたりまで:seleniumでwebサイトに自動入力して検索

--- Inventory_Checker.py
+++ Inventory_Checker.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.techwithtim.net/")
 print(driver.title)
+
+search = driver.find_element_by_name("s")
+search.send_keys("seleniumにより自動入力")
+search.send_keys(Keys.RETURN)
+
+print(driver.page_source)
+
+time.sleep(5)
+
 driver.quit()
