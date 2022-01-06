34f16748eb2704dabd5876f392ca090e4ff19df3
前田 <sloth19920731@gmail.com>
2022-01-06 22:40:15 +0900

    #2の最後まで:スクレイピングしたらchrome閉じる

--- Inventory_Checker.py
+++ Inventory_Checker.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -8,12 +10,21 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.techwithtim.net/")
 print(driver.title)
 
-search = driver.find_element_by_name("s")
-search.send_keys("seleniumにより自動入力")
+search = driver.find_element(by=By.NAME, value="s")
+search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
-print(driver.page_source)
+try:
+    main = element = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.ID,"main"))
+    )
+    articles = main.find_elements(by=By.TAG_NAME, value="article")
+    for article in articles:
+        header = article.find_element(by=By.CLASS_NAME, value="entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
 
-time.sleep(5)
+main = driver.find_element(by=By.ID, value="main")
 
 driver.quit()
