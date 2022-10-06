335e6852b8bc4d7ddb8978ca17247ecafe2ad997
前田 <sloth19920731@gmail.com>
2022-10-06 23:53:39 +0900

    ポケカプレーヤーズログイン

--- Inventory_Checker.py
+++ Inventory_Checker.py
@@ -3,23 +3,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+driver.maximize_window()
 
-driver.get("https://www.techwithtim.net/")
-link = driver.find_element(By.LINK_TEXT, value="Python Programming")
-link.click()
-
-try:
-    element = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.LINK_TEXT,"Beginner Python Tutorials"))
-    )
-    element.click()
-
-    element = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.ID,"sow-button-19310003"))
-    )
-    element.click()
-except:
-    driver.quit()
+driver.get("https://players.pokemon-card.com/login?redirect=/mypage")
+formMailAddress = driver.find_element_by_id("formMailAddress")
+formMailAddress.send_keys('ここにアドレス@gmail.com')
+formPassword = driver.find_element_by_class_name("password")
+formPassword.send_keys('ここにパスワード')
+button = driver.find_elements_by_css_selector('.c-btn')
+time.sleep(2)
+button[2].click()
+time.sleep(1)
